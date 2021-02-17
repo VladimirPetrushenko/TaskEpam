15ad730c390880217622a90e20eb1e48def2ca50
VladimirPetrushenko <VladimirVPetrushenko>
2021-02-17 14:48:41 +0300

    Done task 11

---
+++ 04-Collections/Task 11/ConsoleApp11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var f in new DirectoryInfo(@"D:\обучение\2\Kursovoi\Kursovoi").GetFiles("*.c", SearchOption.AllDirectories))
+            {
+                string s = File.ReadAllText(f.FullName);
+                File.WriteAllText(f.FullName, s, Encoding.UTF8);
+            }
+        }
+    }
+}
